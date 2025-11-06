e56b45889204feb3184d2b37eb0f6ebc83d40697
Samprith Kalakata <ksamprith@gmail.com>
2025-11-05 19:43:22 -0500

    Added RollingUpdate strategy and changed heading in app.py to build image 2.2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ todos = db.todo #Select the collection
 
 app = Flask(__name__)
 title = "TODO with Flask"
-heading = "ToDo Reminder"
+heading = "ToDo Reminder App Version 2.2 (we testing rolling updates!)"
 #modify=ObjectId()
 
 def redirect_url():
